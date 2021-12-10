b4b5f8336f086f9416d5b3054b783b5e38c88fcb
Daniil <coolswood@yandex.ru>
2021-12-10 18:05:41 +0200

    update

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -11,9 +11,33 @@ export default () => {
     <Page>
       <div className={style.productLine}>
         <div className={style.container}>
-          <img className={style.productPic} src={prod} alt="" />
-          <div>Алвовизан</div>
-          <div>Диеногест 2 мг</div>
+          <img
+            style={{
+              marginBottom: 20,
+            }}
+            className={style.productPic}
+            src={prod}
+            alt=""
+          />
+          <div
+            style={{
+              color: '#432E33',
+              marginBottom: 5,
+              fontSize: 30,
+              fontWeight: 'bold',
+            }}
+          >
+            Алвовизан
+          </div>
+          <div
+            style={{
+              color: '#432E33',
+              marginBottom: 20,
+              fontSize: 18,
+            }}
+          >
+            Диеногест 2 мг
+          </div>
           <div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
             aspernatur deleniti doloremque et facilis impedit magni nam quam qui
@@ -22,8 +46,19 @@ export default () => {
           </div>
         </div>
       </div>
-      <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+        }}
+      >
         <Link to={'/prezentation/1'} className={styles.mainBtn}>
+          Презентация
+        </Link>
+        <Link to={'/prezentation/2'} className={styles.mainBtn}>
+          Презентация
+        </Link>
+        <Link to={'/prezentation/10'} className={styles.mainBtn}>
           Презентация
         </Link>
       </div>
